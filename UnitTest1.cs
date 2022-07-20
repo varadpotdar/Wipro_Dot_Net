03cde6c9caca3ac646afe53b6c865eaa2f27fc04
varadpotdar <81146874+varadpotdar@users.noreply.github.com>
2022-07-20 14:55:52 +0530

    Selenium Webdriver

---
+++ UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+
+namespace SeleniumWebdriver
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+           // IWebDriver driver = new ChromeDriver();
+            IWebDriver driver = new InternetExplorerDriver();
+            driver.Navigate().GoToUrl("http://localhost:62098/Login.aspx");
+            driver.Close();
+            driver.Quit();
+        }
+    }
+}
